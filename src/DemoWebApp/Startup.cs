75489da744270e60263b134ca41536cac4a5c46a
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2019-10-13 00:44:18 +0100

    Relative paths behind proxy fix (#2)
    * Add Request.PathBase for proxy
    
    * Conditionally add PathBase

--- src/DemoWebApp/Startup.cs
+++ src/DemoWebApp/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -43,6 +44,16 @@ namespace DemoWebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 // app.UseHsts();
             }
+
+            app.Use((context, next) =>
+            {
+                var forwardedForValue = context.Request.Headers.FirstOrDefault(x => x.Key == "X-Forwarded-For").Value.FirstOrDefault();
+                if (!string.IsNullOrWhiteSpace(forwardedForValue))
+                {
+                    context.Request.PathBase = new PathString("/demowebapp");
+                }
+                return next();
+            });
 
             // app.UseHttpsRedirection();
             app.UseStaticFiles();
